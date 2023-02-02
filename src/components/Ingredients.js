6541d502924ff4ded3032f61d0a0d32fb0c81847
ashanti <ammanier2@gmail.com>
2023-02-02 00:28:18 -0500

    added Ingredient component

---
+++ src/components/Ingredients.js
@@ -0,0 +1,13 @@
+import {useState} from 'react'
+
+function Ingredients() {
+  const [ingredients, setIngredients] = useState("")
+  return (
+    <div>
+      Ingredients
+
+    </div>
+  )
+}
+
+export default Ingredients
