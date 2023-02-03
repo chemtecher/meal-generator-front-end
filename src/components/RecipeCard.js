7c2dcb5697eb38367aceed2575e82a684ff67ab5
ashanti <ammanier2@gmail.com>
2023-02-03 13:22:02 -0500

    added mock random recipes data

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -7,8 +7,8 @@ function RecipeCard(props) {
       <br/>
       <img className="recipe-img" src={image} alt="food dish"/>
       <h2>{title}</h2>
-      <p> clock image {time}</p>
       <p>plate or fork image {servings}</p>
+      <p>clock image {time}</p>
     </div>
   )
 }
